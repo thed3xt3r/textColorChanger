ba1dae12a4c8707cb8b1eb88f6363a0caa65ce0a
thed3xt3r <56760441+thed3xt3r@users.noreply.github.com>
2022-01-03 00:44:48 +0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,20 @@
+function buttonPeach()
+{
+    var txt = document.getElementById("input").value;
+    document.getElementById("textgoeshere").innerHTML = txt;
+    document.getElementById("textgoeshere").style.color = "#F8B195";
+}
+
+function buttonSalmon()
+{
+    var txt = document.getElementById("input").value;
+    document.getElementById("textgoeshere").innerHTML = txt;
+    document.getElementById("textgoeshere").style.color = "#F67280";
+}
+
+function buttonLime()
+{
+    var txt = document.getElementById("input").value;
+    document.getElementById("textgoeshere").innerHTML = txt;
+    document.getElementById("textgoeshere").style.color = "#A8E6CE";
+}
